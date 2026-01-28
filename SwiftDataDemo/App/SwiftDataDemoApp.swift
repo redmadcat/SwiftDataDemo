e7c351a04cdc5dec0e8081705361d59a9a8bc253
Roman Yaschenkov <ryaschenkov@yandex.ru>
2026-01-28 15:29:34 +0300

    init: initial commit
    Initial commit for SwiftData demo application.

---
+++ SwiftDataDemo/App/SwiftDataDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataDemoApp.swift
+//  SwiftDataDemo
+//
+//  Created by Roman Yaschenkov on 28.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
