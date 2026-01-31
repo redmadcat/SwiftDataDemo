ab47027e51d73026ab09f4b1074f87a585e76951
Roman Yaschenkov <ryaschenkov@yandex.ru>
2026-01-31 16:31:17 +0300

    add: content view layout impl
    Add ContentView TaskList layout implementation.

--- SwiftDataDemo/ContentView.swift
+++ SwiftDataDemo/ContentView.swift
@@ -6,19 +6,74 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    @Environment(\.modelContext) private var modelContext
+    @State private var newTaskIsShow = false
+    @Query(sort: \TaskItem.priorityValue, order: .reverse) private var taskItems: [TaskItem]
+        
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            VStack {
+                HStack {
+                    Text("Task List")
+                        .font(.system(size: 40, weight: .black, design: .rounded))
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        newTaskIsShow = true
+                    }) {
+                        Image(systemName: "plus.circle.fill")
+                            .font(.largeTitle)
+                            .foregroundStyle(.purple)
+                    }
+                }
+                .padding([.top, .leading, .trailing])
+                
+                taskItems.count == 0 ?
+                    AnyView(NoDataView()) :
+                    AnyView(
+                        List {
+                            ForEach(taskItems) { taskItem in
+                                TaskListRowView(taskItem: taskItem)
+                            }
+                            .onDelete(perform: deleteTask)
+                        }
+                        .listStyle(.plain))
+            }
+        }.sheet(isPresented: $newTaskIsShow) {
+            TaskCreationView(isShow: $newTaskIsShow)
+                .presentationDetents([.medium])
         }
-        .padding()
+    }
+    
+    private func deleteTask(indexSet: IndexSet) {
+        for index in indexSet {
+            let itemToDelete = taskItems[index]
+            modelContext.delete(itemToDelete)
+        }
     }
 }
 
 #Preview {
     ContentView()
+        .modelContainer(previewContainer)
 }
+
+@MainActor
+let previewContainer: ModelContainer = {
+    do {
+        let container = try ModelContainer(for: TaskItem.self, configurations: ModelConfiguration(isStoredInMemoryOnly: true))
+        
+        for index in 0..<5 {
+            let newItem = TaskItem(name: "Task item #\(index)")
+            container.mainContext.insert(newItem)
+        }
+        
+        return container
+    } catch {
+        fatalError("Failed to create container")
+    }
+}()
